8cf5a5afca47c6131a8f2a0e2568adc82b820e58
Guido D'Orsi <guido@float.com>
2024-06-04 19:41:47 +0200

    feat: improve audio management

---
+++ src/hooks/useMediaPlayer.ts
@@ -0,0 +1,43 @@
+import { useMemo, useRef, useState } from "react";
+import { AudioManager } from "../lib/AudioManager";
+
+export type PlayState = "pause" | "play";
+
+export function useMediaPlayer() {
+  const audioManager = useMemo(() => new AudioManager(), []);
+
+  const [currentMedia, setCurrentMedia] = useState<string>("");
+  const [playState, setPlayState] = useState<PlayState>("pause");
+
+  const previousMediaLoad = useRef(Promise.resolve());
+
+  async function playMedia(fileName: string) {
+    // Wait for the previous load to finish
+    // to avoid to incur into concurrency issues
+    await previousMediaLoad.current;
+
+    const promise = audioManager.loadAudio(fileName);
+    previousMediaLoad.current = promise;
+
+    audioManager.play();
+    setPlayState("play");
+    setCurrentMedia(fileName);
+  }
+
+  async function togglePlayState() {
+    if (playState === "pause") {
+      audioManager.play();
+      setPlayState("play");
+    } else {
+      audioManager.pause();
+      setPlayState("pause");
+    }
+  }
+
+  return {
+    currentMedia,
+    playState,
+    playMedia,
+    togglePlayState,
+  };
+}
