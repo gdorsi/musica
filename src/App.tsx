08fa69f1de26d2265a87eaff5a655203dff683e2
paolodo <pa.dorsi@gmail.com>
2024-06-06 22:37:05 +0200

    implemented basic layout with shadcn/ui and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import type { Documents, MusicCollectionDocument } from "./state/repository";
 import { copyToPrivateFileSystem } from "./lib/filesystem";
 import { useMediaPlayer } from "./hooks/useMediaPlayer";
-
+import { Button } from "./ui/components/ui/button";
+import { PlusCircledIcon } from "@radix-ui/react-icons";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "./ui/components/ui/carousel";
+import { Card, CardContent } from "./ui/components/ui/card";
+import waveform from "@/ui/waveform.png";
+import { FaPause, FaPlay } from "react-icons/fa";
 function App(props: { documents: Documents }) {
 	const mediaPlayer = useMediaPlayer();
 	const [doc, change] = useDocument<MusicCollectionDocument>(
@@ -33,39 +44,76 @@ function App(props: { documents: Documents }) {
 
 	return (
 		<>
-			<label style={{ position: "fixed", right: 10, top: 10 }}>
-				<input type="file" onChange={handleFileLoad} multiple />
-			</label>
-			<div
-				// TODO: Select the styles library (vanilla extract?)
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "center",
-					width: "100%",
-					height: "100%",
-				}}
-			>
-				My collection:{" "}
-				{doc?.collection.map((item) => (
-					<button
-						type="button"
-						key={item.fileName}
-						onClick={() => mediaPlayer.playMedia(item.fileName)}
-						disabled={item.fileName === mediaPlayer.currentMedia}
-					>
-						{item.title}
-					</button>
-				))}
+			<section className="w-full mt-0  bg-white flex flex-row   justify-between pt-0 pr-5 pb-5 border-b border-zinc-200">
+				<div />
+				<div className="text-xl font-semibold text-zinc-900">
+					My collection:{" "}
+				</div>
+				<div className="">
+					<Button className="hover:pointer">
+						<label className="flex flex-row hover:pointer">
+							<input type="file" onChange={handleFileLoad} multiple hidden />
+							<PlusCircledIcon className="mr-2 h-4 w-4 hover:pointer" />
+							add files
+						</label>
+					</Button>
+				</div>
+			</section>
+
+			<div className=" flex flex-col items-center p-6">
+				<Carousel className="w-full max-w-sm justify-between">
+					<CarouselContent>
+						{doc?.collection.map((item) => (
+							<div key={item.fileName} className="flex flex-col items-center ">
+								<CarouselItem className="md:basis-1/3 lg:basis-1/5 w-50">
+									<label>
+										<button
+											type="button"
+											key={item.fileName}
+											onClick={() => mediaPlayer.playMedia(item.fileName)}
+											disabled={item.fileName === mediaPlayer.currentMedia}
+											hidden
+										/>
+										<Card className="h-40 w-[100px] hover:cursor-pointer">
+											<CardContent className="flex aspect-square items-center justify-center p-0">
+												<img
+													src={waveform}
+													alt="track"
+													className="h-full w-full"
+												/>
+											</CardContent>
+										</Card>
+									</label>
+								</CarouselItem>
+								{item.title}
+							</div>
+						))}
+					</CarouselContent>
+					<CarouselPrevious />
+					<CarouselNext />
+				</Carousel>
 			</div>
-			<button
-				type="button"
-				onClick={mediaPlayer.togglePlayState}
-				disabled={!mediaPlayer.currentMedia}
-			>
-				{mediaPlayer.playState === "pause" ? "Play" : "Pause"}
-			</button>
+
+			<section className="w-full mt-0  flex flex-row   justify-between pt-6 border-t border-grey-200">
+				<div />
+				<label>
+					<button
+						className="b-0  "
+						type="button"
+						onClick={mediaPlayer.togglePlayState}
+						disabled={!mediaPlayer.currentMedia}
+						hidden
+					/>
+					<div>
+						{mediaPlayer.playState === "pause" ? (
+							<FaPlay size={70} className="hover:cursor-pointer" />
+						) : (
+							<FaPause size={70} className="hover:cursor-pointer" />
+						)}
+					</div>
+				</label>
+				<div />
+			</section>
 		</>
 	);
 }
