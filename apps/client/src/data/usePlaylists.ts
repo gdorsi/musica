af32e1b670456ed0e4f5ed882fdaf3d5046a2bcc
Guido D'Orsi <guido@float.com>
2024-06-21 14:44:26 +0200

    chore: playlist hook

---
+++ apps/client/src/data/usePlaylists.ts
@@ -0,0 +1,35 @@
+import { useDocuments, useRepo } from "@automerge/automerge-repo-react-hooks";
+import { Playlist, PlaylistVersion, type MusicCollection } from "./schema";
+import { useRootDocument } from "@/auth/useRootDocument";
+
+export function usePlaylists() {
+	const repo = useRepo();
+	const [rootDocument, change] = useRootDocument();
+
+	const playlists = useDocuments<MusicCollection>(
+		rootDocument?.playlists ?? [],
+	);
+
+	async function createPlaylist(name: string) {
+		if (!rootDocument) return;
+
+		const handle = repo.create<Playlist>({
+			id: crypto.randomUUID(),
+			items: [],
+			name,
+			owner: rootDocument?.owner,
+			version: PlaylistVersion,
+		});
+
+		change((rootDocument) => {
+			rootDocument.playlists.push(handle.url);
+		});
+	}
+
+	return {
+		createPlaylist,
+		playlists,
+	};
+}
+
+export type PlaylistsApi = ReturnType<typeof usePlaylists>;
